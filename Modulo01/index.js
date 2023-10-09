7bd9351f830c7a2a005550198bcbf03b61624c22
gabrielpoke <gabriel_victor_prestes@hotmail.com>
2023-10-08 23:36:46 -0300

    Entendendo Query e Route params

--- Modulo01/index.js
+++ Modulo01/index.js
@@ -2,9 +2,22 @@ const express = require('express');
 
 const server = express();
 
+
+//Query params = Parametros passados na frente das rotas como por exemplo ?nome=Nodejs
+
+//Route params = Parametros passados diretamente na rota para acessar recursos cursos/2
+
+//Request Body = Quando mandamos um objeto no corpo da requisção {cursos:"Node", tipo : "a"}
+
 //req = Representa dados da nossa requisição
 
 //res = Representa dados e informações da nossa resposta
-server.get('/cursos', (req, res)=>{ return res.json({"curso":"NodeJs"})})
+server.get('/cursos/:id', (req, res)=>{ 
+
+  const id = req.params.id;
+
+  return res.json({"curso": `Aprendendo ${id}`})
+
+})
 
 server.listen(3000);
